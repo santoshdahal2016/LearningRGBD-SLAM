e7a983fa759739c531c5ab311198b536333354a9
santoshdahal2016 <webtutor2016@gmail.com>
2020-02-11 17:06:22 +0545

    second chapter complete

--- src/generatePointCloud.cpp
+++ src/generatePointCloud.cpp
@@ -1,6 +1,7 @@
 // C ++ Standard Library 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 // OpenCV library 
@@ -31,14 +32,20 @@ int main( int argc, char** argv )
     cv::Mat rgb, depth;
     // Use cv :: imread () To read image
     // API: http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#cv2.imread
-    rgb = cv::imread( "./data/rgb.png" );
+    rgb = cv::imread( "../data/rgb.png" );
     // The rgb image is a color image of 8UC3
     // The depth is a single-channel image of 16UC1. Note that the flags setting is -1, which means that the original data is read without any modification. 
-    depth = cv::imread( "./data/depth.png", -1 );
+    depth = cv::imread( "../data/depth.png", -1 );
 
+
+	cv::namedWindow("Depth Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Depth Image", depth);
+    cv::waitKey(0);
     // Point cloud variable
     // Use smart pointer to create an empty point cloud. This pointer will be released automatically when used up. 
     PointCloud::Ptr cloud ( new PointCloud );
+  
+  	cout<<"-> Before loop of reading depth."<<endl;
 
     // traverse the depth map 
     for (int m = 0; m < depth.rows; m++)
@@ -64,14 +71,19 @@ int main( int argc, char** argv )
             p.r = rgb.ptr<uchar>(m)[n*3+2];
 
             // Add p to the point cloud in 
-            cloud->points.push_back( p );
+            cloud->points.push_back(p);
         }
     // set and saved point cloud 
+    cout<<"-> After loop"<<endl;
     cloud->height = 1;
     cloud->width = cloud->points.size();
     cout<<"point cloud size = "<<cloud->points.size()<<endl;
     cloud->is_dense = false;
+    cout<<"-> before saving"<<endl;
+
     pcl::io::savePCDFile( "./pointcloud.pcd", *cloud );
+    cout<<"-> after saving"<<endl;
+
     // Clear the data and exit 
     cloud->points.clear();
     cout<<"Point cloud saved."<<endl;
